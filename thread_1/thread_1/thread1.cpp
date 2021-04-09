09d4709303fedd44460eaf39596b3cfc074db3b9
Jo <jkyu6554@kpu.ac.kr>
2021-04-09 15:12:02 +0900

    쓰레드 함수인자값 넣기

--- thread_1/thread_1/thread1.cpp
+++ thread_1/thread_1/thread1.cpp
@@ -3,12 +3,17 @@
 
 void do_thread() 
 {
-	std::cout << "Iam a anoter thread\n";
+	std::cout << "Iam a anoter thread: "<<std::this_thread::get_id()<<"\n";
 }
 
 int main()
 {
-	std::thread t1{do_thread};
-	std::cout << "hello world\n";
+
+	std::thread t1{ do_thread };
+	std::thread t2{ do_thread };
+	std::thread t3{ do_thread };
+	std::thread t4{ do_thread };
+	std::thread t5{ do_thread };
+
 	t1.join();
 }
