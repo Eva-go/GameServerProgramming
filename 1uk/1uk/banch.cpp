ac2e47fdce9e9a40ce38222ceea26bd94012f990
Jo <jkyu6554@kpu.ac.kr>
2021-04-14 16:14:22 +0900

    다중 멀티 쓰레드 테스트

--- 1uk/1uk/banch.cpp
+++ 1uk/1uk/banch.cpp
@@ -2,34 +2,42 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <vector>
 using namespace std;
 using namespace chrono;
+
+#define MAX_THREADS 16
 
 constexpr int LOOP_COUNT = 500000000;
 volatile int sum = 0;
 
-void worker()
+void worker(int num_threads)
 {
-	int loop = LOOP_COUNT / 2;
+	int loop = LOOP_COUNT / num_threads;
 	for (int i = 0; i < loop; ++i)
 		sum = sum + 2;
 }
 
 int main()
 {
+	vector <thread> workers;
 	//나만 안배운 volatile
+	for (int num_threads = 1; num_threads <= MAX_THREADS; num_threads *= 2) {
+		sum = 0;
+		workers.clear();
+		auto start_t = high_resolution_clock::now();
+		
+		for (int i = 0; i < num_threads; ++i)
+			workers.emplace_back(worker,num_threads);
+		for (auto& th : workers)
+			th.join();
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+		cout << num_threads << "threads, ";
+		cout << "Exec Time = " << duration_cast<milliseconds>(exec_t).count() << "ms";
+		cout << "Result = " << sum << endl;
+	}
 	
-	auto start_t = high_resolution_clock::now();
-	//thread t1{ worker };
-	//thread t2{ worker };
-	//t1.join();
-	//t2.join();
-	worker();
-	worker();
 
-	auto end_t = high_resolution_clock::now();
-	auto exec_t = end_t - start_t;
 
-	cout << "Exec Time = " << duration_cast<milliseconds>(exec_t).count() <<"ms"<< endl;
-	cout << "Result = " << sum << endl;
 }
