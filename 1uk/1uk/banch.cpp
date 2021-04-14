c403c66763718df9a7447510cfcbac709779ff57
Jo <jkyu6554@kpu.ac.kr>
2021-04-14 16:02:23 +0900

    멀티 스레드 테스트

--- 1uk/1uk/banch.cpp
+++ 1uk/1uk/banch.cpp
@@ -1,17 +1,29 @@
 //밴치마크테스트
 #include <iostream>
 #include <chrono>
+#include <thread>
 using namespace std;
 using namespace chrono;
 
 constexpr int LOOP_COUNT = 500000000;
+volatile int sum = 0;
+
+void worker()
+{
+	int loop = LOOP_COUNT / 2;
+	for (int i = 0; i < loop; ++i)
+		sum = sum + 2;
+}
+
 int main()
 {
 	//나만 안배운 volatile
-	volatile int sum = 0;
+	
 	auto start_t = high_resolution_clock::now();
-	for (int i = 0; i < LOOP_COUNT; ++i)
-		sum = sum + 2;
+	thread t1{ worker };
+	thread t2{ worker };
+	t1.join();
+	t2.join();
 	auto end_t = high_resolution_clock::now();
 	auto exec_t = end_t - start_t;
 
