2f7e09cbe06bb707f4f76263634c61bc0a420caf
jo <whdudghks22@gmail.com>
2021-04-23 14:11:06 +0900

    while 쪽 lock이 걸러져있기 때문에 bool값을 변경불가

--- thread_sync/thread_sync/thread_sync.cpp
+++ thread_sync/thread_sync/thread_sync.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <thread>
-
+#include <mutex>
 //버그를 보려면 버전16.8을 사용
 
 using namespace std;
 
+mutex m;
 int g_data = 0;
-volatile bool g_ready = false;
+bool g_ready = false;
 
 void receiver()
 {
+	m.lock();
 	while (false == g_ready);
+	m.unlock();
 	cout << "I received " << g_data << "\n";
 }
 
 void sender()
 {
 	g_data = 999;
+	m.lock();
 	g_ready = true;
+	m.unlock();
 }
 
 int main()
